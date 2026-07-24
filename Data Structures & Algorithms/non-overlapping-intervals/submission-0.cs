f30b657b224f2d6182eb0a2b52bc6793abdca794
mrudulagem <mrudulagem@gmail.com>
2026-07-24 13:30:10 -0400

    Add: non-overlapping-intervals - submission-0

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-0.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int EraseOverlapIntervals(int[][] intervals) {
+        if(intervals.Length < 2) {
+            return 0;
+        }
+
+        Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
+        int overlap = 0;
+        var prev = intervals[0];
+        for(int i = 1; i < intervals.Length; i++) {
+            var curr = intervals[i];
+            if(curr[0] < prev[1]) {
+                overlap++;
+                prev[1] = Math.Min(prev[1], curr[1]);
+            } else {
+                // curr[0] >= prev[1];
+                prev[1] = curr[1];
+            }
+        }
+
+        return overlap;
+    }
+}
